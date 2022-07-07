27911588354ffa0b4f23ef643c7d5c0ac7f9bb02
Tim Gage <tsg21@users.noreply.github.com>
2022-07-07 18:21:33 +0100

    Remove data fetching logging

--- custom_components/volkswagen_we_connect_id/__init__.py
+++ custom_components/volkswagen_we_connect_id/__init__.py
@@ -42,9 +42,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     async def async_update_data():
         """Fetch data from Volkswagen API."""
-        _LOGGER.info("Fetching data from VW API...")
         await hass.async_add_executor_job(_we_connect.update)
-        _LOGGER.info("Data retrieved")
         vehicles = []
 
         for vin, vehicle in _we_connect.vehicles.items():
