7d14b2ba57fe4613b43b243b2559fc2f85b2ed10
Tim Gage <tsg21@users.noreply.github.com>
2022-07-07 18:22:54 +0100

    Revert changes

--- custom_components/volkswagen_we_connect_id/__init__.py
+++ custom_components/volkswagen_we_connect_id/__init__.py
@@ -43,6 +43,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     async def async_update_data():
         """Fetch data from Volkswagen API."""
         await hass.async_add_executor_job(_we_connect.update)
+
         vehicles = []
 
         for vin, vehicle in _we_connect.vehicles.items():
@@ -50,7 +51,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                 vehicles.append(vehicle)
 
         hass.data[DOMAIN][entry.entry_id + "_vehicles"] = vehicles
-        
         return vehicles
 
     coordinator = DataUpdateCoordinator(
