4d4405ff74fb4ba56ece40aa274e04c3d4352c62
gilder0 <26692162+gilder0@users.noreply.github.com>
2023-08-31 22:28:46 +0200

    Add Supported : Tiguan PHEV

--- custom_components/volkswagen_we_connect_id/__init__.py
+++ custom_components/volkswagen_we_connect_id/__init__.py
@@ -29,7 +29,7 @@ PLATFORMS = [
 
 _LOGGER = logging.getLogger(__name__)
 
-SUPPORTED_VEHICLES = ["ID.3", "ID.4", "ID.5", "ID. Buzz"]
+SUPPORTED_VEHICLES = ["ID.3", "ID.4", "ID.5", "ID. Buzz", "Tiguan"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
